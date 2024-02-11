19ad09c8a72e2611b3c174cf1463183064a3049c
Emily Roys <emilykristelroys@gmail.com>
2024-02-11 20:10:46 +0000

    Update home page

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,9 +1,17 @@
 import '../styles/Content.css'; 
+import plant from '../assets/plant.png';
 
 const Content = () => {
     return (
-        <div>
-            
+        <div className='content'>
+            <div className= 'text-content'>
+                <h1 className="line1">Your Garden,</h1>
+                <h1 className="line2">Your Food,</h1>
+                <h1 className="line3">Our Help.</h1>
+            </div>
+            <div className= 'image-content'>
+                <img src={plant} alt='plant' className= 'plant'/>
+            </div>
         </div>
     );
 
