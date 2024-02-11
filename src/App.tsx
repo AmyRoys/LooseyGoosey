97ec16ae9c09b931d1dbbab7696a8e9d8ff9d372
Emily Roys <emilykristelroys@gmail.com>
2024-02-11 20:12:09 +0000

    Add background color & page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import './App.css'
-import Content from './components/Content'
-import NavBar from './components/NavBar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About'; 
+import Login from './components/Login'; 
+import Content from './components/Content';
 
 function App() {
-
   return (
-    <>
-     <div>
-          <NavBar />
-          <Content />
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Content />} />
+        </Routes>
+        <Footer/>
       </div>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
