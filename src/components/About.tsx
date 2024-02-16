5d827d446ba5ae9748908d1a7add9638681e6a8d
Emily Roys <emilykristelroys@gmail.com>
2024-02-16 17:04:27 +0000

    Update general stylings & rename header

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,8 +2,8 @@ import '../styles/About.css';
 
 const About: React.FC = () => {
     return (
-        <div className='text-content'>
-        <h2>About</h2>
+        <div className='text-about'>
+        <h1>About</h1>
         <p>We are a company</p>
         </div>
     );
