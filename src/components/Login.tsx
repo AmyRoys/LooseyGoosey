e4a741c4200442233aedbb38c974c0558b743f04
Emily Roys <emilykristelroys@gmail.com>
2024-02-16 17:04:09 +0000

    Update login and content files

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import '../styles/Login.css';
+import plant2 from '../assets/plant2.png';
+import plant3 from '../assets/plant3.png';
 
 const Login: React.FC = () => {
     const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-const [message, setMessage] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
 
 const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -17,13 +19,28 @@ const handleSubmit = (e: React.FormEvent) => {
 
 return(
     <div className='container'>
-        <h2 className='text-content'>Login Page</h2>
-        <form onSubmit={handleSubmit}>
-            <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" required className='text-box'/>
-            <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" required className='text-box' />
-            <button type="submit">Login</button>
-        </form>
-        <p className='text-content'>{message}</p>
+        <div className='login-box'>
+            <div className = 'container-input'>
+                <h1 className='text-login'>Login Page</h1> 
+        
+                <form onSubmit={handleSubmit}>
+                    <div>
+                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" required className='text-box'/>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" required className='text-box' />
+                    </div>
+                    <div>
+                    <button type="submit">Submit</button>
+                    </div>
+                </form>
+            <p className='message'>{message}</p>
+            </div>
+        </div>
+        <div>
+            <img src={plant2} alt='plant2' className='plant2'/>
+        </div>
+        <div>
+            <img src={plant3} alt='plant3' className='plant3'/>
+        </div>
     </div>
 )
 }
