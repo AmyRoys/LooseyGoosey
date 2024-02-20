41d1706ece2f3b6490f394d3626f9b2934f5df6a
Emily Roys <emilykristelroys@gmail.com>
2024-02-20 20:36:59 +0000

    Add stylings and subheadings to about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,11 +2,19 @@ import '../styles/About.css';
 
 const About: React.FC = () => {
     return (
-        <div className='text-about'>
-        <h1>About</h1>
-        <p>We are a company</p>
+        <div className='about-container'>
+            <div className='about-box'>
+                <div className='container-about'>
+                    <h1 className='text-about'>About</h1>
+                    <h2 className='text-divider'>Broad User Group</h2>
+                    <p>The broad user group I chose is..</p>
+                    <h2 className='text-divider'>Segmentation Method</h2>
+                    <p>The segmentation method I chose is..</p>
+                    <h2 className='text-divider'>User Persona</h2>
+                    <p>My ideal user is...</p>
+                </div>
+            </div>
         </div>
     );
 }
-
 export default About;
