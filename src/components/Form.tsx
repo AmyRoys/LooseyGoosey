89f20e8c86170f0dda712bb492368c5f9dd3bf1b
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2024-03-14 15:18:54 +0000

    fix error

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,7 +8,7 @@ function Form() {
   const [message, setMessage] = useState('');
   const [file, setFile] = useState(null);
 
-  const handleFileChange = (event: { target: { files: React.SetStateAction<null>[]; }; }) => {
+  const handleFileChange = (event: any) => {
     setFile(event.target.files[0]);
   };
 
