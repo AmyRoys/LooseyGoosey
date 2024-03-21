021004ee37f198d384c8cb1e2ad8c39508f60dd5
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2024-03-21 14:56:23 +0000

    add table with plant data -working

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -1,62 +1,52 @@
 import axios from "axios";
+import {Table} from 'react-bootstrap';
+import '../styles/Data.css';
 
 import { useState, useEffect } from "react";
 const apiurl = "https://7je6jjr9ka.execute-api.us-east-1.amazonaws.com/Beta/";
 
-interface PlantData {
-  soilMoisture: number;
-  lightIntensity: number;
-  temperature: number;
-}
-
-async function getPlantData(): Promise<PlantData[]> {
-  try {
-    const response = await axios.get(apiurl);
-    console.log("Success");
-    return response.data;
-  } catch (error) {
-    console.log("Error", error);
-    return [];
+async function getPlantData(): Promise<any[]> {
+    try {
+      const response = await axios.get(apiurl);
+      console.log("Success");
+      const data = JSON.parse (response.data.body);
+      console.log( data);
+      return data;
+    } catch (error) {
+      console.log("Error", error);
+      return [];
+    }
   }
-}
-
-function DataContent() {
-  const [plantData, setPlantData] = useState<PlantData[]>([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getPlantData();
-      setPlantData(data);
-    };
-    fetchData();
-  }, []);
-
-  const data = plantData.map((plant) => ({
-    soilMoisture: plant.soilMoisture,
-    lightIntensity: plant.lightIntensity,
-    temperature: plant.temperature,
-  }));
-  console.log("data:", data);
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th> Time Stamp</th>
-          <th> Soil Moisture</th>
-          <th> Light Intensity</th>
-          <th> Temperature</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
-            <td>{item.soilMoisture}</td>
-            <td>{item.lightIntensity}</td>
-            <td>{item.temperature}</td>
+  
+  function DataContent() {
+    const [data, setData] = useState<any[]>([]);
+  
+    useEffect(() => {
+      getPlantData().then(setData);
+    }, []);
+  
+    return (
+      <Table className="myTable" striped bordered hover>
+        <thead>
+          <tr>
+            <th>Timestamp</th>
+            <th>Light Value</th>
+            <th>Soil Value</th>
+            <th>Temp Value</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.TimeStamp}</td>
+              <td>{item.lightValue}</td>
+              <td>{item.soilValue}</td>
+              <td>{item.tempValue}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    );
+  }
 
 export default DataContent;
