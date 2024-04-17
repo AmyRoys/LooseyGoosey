61b494ec3134296317eb23ed1dd18fb34b6bed69
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2024-04-17 17:15:46 +0100

    add convention to events page

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,10 +1,20 @@
 import '../styles/Events.css';
+import Goose from '../assets/images/sillygeese.png';
+import { Button } from 'react-bootstrap';
+
 function Events() {
   
   
   return (
     <div>
-        
+        <h1 className = 'h'> Upcoming Events</h1>
+        <div className= 'event'> 
+            <h2>Silly Goose Convention</h2>
+            <h3 className = 'datetime'>2nd March</h3>
+            <h3 className='datetime'>10:00 AM</h3>
+            <img className= 'geese' src ={Goose} ></img>
+            <Button className = 'button'>Tickets</Button>
+        </div>
     </div>
 
   );
