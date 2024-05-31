fc5825a3f5e5ba4ea155a1f997e0b983e6b1a0a4
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2024-05-31 14:02:32 +0100

    re-deploy contract to sepolia network

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,34 +1,52 @@
 require("@nomiclabs/hardhat-waffle");
 
-const fs = require('fs');
-//const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+const fs = require("fs");
+const infuraId =
+  "https://sepolia.infura.io/v3/c38025e078f74cf99667aa3fa5268998";
+const privateKey =
+  "0x52b7ff963067c53efe60f15a4bb1266c512759bbbe076956db71099597f6538f";
 
-// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
-const infuraId = "your_infura_id";
-const privateKey = "0xa6b8739d185b5e27e1d3f8c8c149f1285f0819bfbb552c7f09c407b96f708853";
+task(
+  "check-balance",
+  "Prints the balance of the deployer account",
+  async (taskArgs, hre) => {
+    const [deployer] = await hre.ethers.getSigners();
+    const balance = await hre.ethers.provider.getBalance(deployer.address);
+    console.log("Account balance:", balance.toString());
+  }
+);
+
+async function estimateGas() {
+  const ContractFactory = await ethers.getContractFactory("YourContract");
+  const estimatedGas = await ContractFactory.deploy().estimateGas();
+  console.log(`Estimated gas: ${estimatedGas}`);
+}
 
 module.exports = {
-  defaultNetwork: "ganache",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
     localhost: {
       url: "http://127.0.0.1:8545",
     },
     ganache: {
-      url: "http://127.0.0.1:7545", // Ganache GUI
-      accounts: [privateKey]
-    }
+      url: "http://127.0.0.1:8545",
+      accounts: [privateKey],
+    },
+    sepolia: {
+      url: infuraId,
+      accounts: [privateKey],
+    },
   },
   solidity: {
     version: "0.8.4",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
   paths: {
     sources: "./contracts",
