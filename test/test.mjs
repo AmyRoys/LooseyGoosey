daf68e3ab0a459a4f2b411c59784a52971272963
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2024-06-05 12:05:54 +0100

    Add more tests

--- test/test.mjs
+++ test/test.mjs
@@ -4,13 +4,15 @@ import * as chai from "chai";
 import { solidity } from "ethereum-waffle";
 chai.use(solidity);
 const { expect } = chai;
+// list of local Eth accounts
+const [recipient] = await ethers.getSigners();
 
+// tests for buying tickets using the contract
 describe("Tickets", function () {
   it("Should set the right ticketPriceInWei", async function () {
     const Tickets = await ethers.getContractFactory("Tickets");
     const tickets = await Tickets.deploy(ethers.utils.parseEther("0.01"));
     const actualPrice = await tickets.ticketPriceInWei();
-    console.log("Actual price: ", actualPrice.toString());
     expect(actualPrice).to.equal(ethers.utils.parseEther("0.01"));
   });
 
@@ -19,13 +21,38 @@ describe("Tickets", function () {
     const tickets = await Tickets.deploy(ethers.utils.parseEther("0.01"));
     await tickets.purchaseTicket(1, { value: ethers.utils.parseEther("0.01") });
     const actualBalance = await tickets.balanceOf(await tickets.owner());
-    console.log("Actual balance: ", actualBalance.toString());
     expect(actualBalance).to.equal(ethers.utils.parseEther("1"));
   });
 
   it("Should not allow to purchase tickets without enough Ether", async function () {
     const Tickets = await ethers.getContractFactory("Tickets");
     const tickets = await Tickets.deploy(ethers.utils.parseEther("0.01"));
-    await expect(tickets.purchaseTicket(1, { value: ethers.utils.parseEther("0.005") })).to.be.revertedWith("Not enough Ether sent");
+    await expect(
+      tickets.purchaseTicket(1, { value: ethers.utils.parseEther("0.005") })
+    ).to.be.revertedWith("Not enough Ether sent");
+  });
+});
+
+// tests for transfering tickets using the contract
+describe("Transfer", function () {
+  it("Should revert if sender tries to transfer more tickets than they have", async function () {
+    const Tickets = await ethers.getContractFactory("Tickets");
+    const tickets = await Tickets.deploy(ethers.utils.parseEther("0.01"));
+    await tickets.purchaseTicket(1, { value: ethers.utils.parseEther("0.01") });
+    await expect(
+      tickets.transferTicket(recipient.address, ethers.utils.parseEther("2"))
+    ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
+  });
+
+  it("Should revert if sender tries to transfer tickets to the zero address", async function () {
+    const Tickets = await ethers.getContractFactory("Tickets");
+    const tickets = await Tickets.deploy(ethers.utils.parseEther("0.01"));
+    await tickets.purchaseTicket(1, { value: ethers.utils.parseEther("0.01") });
+    await expect(
+      tickets.transferTicket(
+        ethers.constants.AddressZero,
+        ethers.utils.parseEther("1")
+      )
+    ).to.be.revertedWith("ERC20: transfer to the zero address");
   });
 });
